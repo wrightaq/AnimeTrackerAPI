f46c52ad3c5d4fb3d54a46990b7b8c4974990089
Amanda <amandaqwright@gmail.com>
2023-11-02 14:31:35 -0500

    setup apollo server and schema

---
+++ anime-tracker/server/index.js
@@ -0,0 +1,14 @@
+const { ApolloServer } = require("@apollo/server");
+const { startStandaloneServer } = require("@apollo/server/standalone");
+const typeDefs = require("./schema");
+
+async function startApolloServer() {
+  const server = new ApolloServer({ typeDefs });
+  const { url } = await startStandaloneServer(server);
+  console.log(`
+    🚀  Server is running!
+    📭  Query at ${url}
+  `);
+}
+
+startApolloServer();
---
+++ anime-tracker/server/schema.js
@@ -0,0 +1,39 @@
+const gql = require("graphql-tag");
+const typeDefs = gql`
+
+type Query {
+
+}
+
+"Title of an anime"
+  type Title {
+    id: ID
+    "list of characters in the titled anime
+    characters: [String]
+    "link to image of the titles anime"
+    image: String
+  }
+
+"Characters in an anime"
+  type Character {
+    id: ID
+    "title of the anime the character is in"
+    title: String
+    "name of the voice actor for the character"
+    voiceActor: String
+    "link to image of the character"
+    image: String
+  }
+
+"Voice Actor for a character in an anime"
+  type VoiceActor {
+    id: ID
+    "list of characters the voice actor voices"
+    characters: [String]
+    "link to image of the voice actor"
+    image: String
+  }
+
+`;
+
+module.exports = typeDefs;
